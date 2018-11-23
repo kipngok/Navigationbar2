c3485d6b7e8c33569399ff1abdeb6dd742882164
kipngok <kenniken120@gmail.com>
2018-11-23 13:59:59 +0300

    first commit

--- app/src/main/java/com/example/kenni/navgationtrial/Login.java
+++ app/src/main/java/com/example/kenni/navgationtrial/Login.java
@@ -1,35 +1,19 @@
 package com.example.kenni.navgationtrial;
 
-import android.content.ClipData;
-import android.content.Intent;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class Login extends AppCompatActivity {
     private DrawerLayout mDrawerlayout;
     private ActionBarDrawerToggle mToggle;
-    private Button buttonlogin;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        buttonlogin = findViewById(R.id.buttonlogin);
-
-          buttonlogin.setOnClickListener(new View.OnClickListener() {
-          @Override
-         public void onClick(View v) {
-        Intent intent = new Intent(MainActivity.this,Login.class);
-        startActivity(intent);
-    }
-        });
+        setContentView(R.layout.activity_login);
 
         mDrawerlayout=findViewById(R.id.drawer);
         mToggle=new ActionBarDrawerToggle(this,mDrawerlayout,R.string.open,R.string.close);
@@ -37,12 +21,10 @@ public class MainActivity extends AppCompatActivity {
         mToggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-
 
         if (mToggle.onOptionsItemSelected(item));
         {
